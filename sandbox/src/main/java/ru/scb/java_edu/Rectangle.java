0a5af4c2ca87a0f63573075a7bd38f230c4c74d6
Istrane <86684863+Istrane@users.noreply.github.com>
2021-07-05 23:49:09 +0400

    Созданы классы для высчитывания площадей квадрата и прямоугольника

---
+++ sandbox/src/main/java/ru/scb/java_edu/Rectangle.java
@@ -0,0 +1,11 @@
+package ru.scb.java_edu;
+
+public class Rectangle {
+    public double a;
+    public double b;
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    }
+
---
+++ sandbox/src/main/java/ru/scb/java_edu/Square.java
@@ -0,0 +1,8 @@
+package ru.scb.java_edu;
+
+public class Square {
+    public double l;
+    public Square(double l){
+        this.l = l;
+    }
+}
