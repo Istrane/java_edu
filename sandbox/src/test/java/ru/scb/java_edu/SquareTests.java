18cacf175d1be9ae8e7aecb2064ebb25f5ab1fcd
Istrane <86684863+Istrane@users.noreply.github.com>
2021-07-13 21:37:50 +0400

    Первый автотест

---
+++ sandbox/src/test/java/ru/scb/java_edu/SquareTests.java
@@ -0,0 +1,13 @@
+package ru.scb.java_edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+    @Test
+    public void testArea(){
+       Square s = new Square(5);
+       Assert.assertEquals(s.area(), 25);
+    }
+}
